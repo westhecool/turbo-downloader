b8302572b73be6cdb862822609ef16b8514aaea8
westhecool <westhecool@users.noreply.github.com>
2024-10-22 10:17:54 -0400

    fix some bugs

--- turbo-downloader.py
+++ turbo-downloader.py
@@ -6,7 +6,9 @@ import math
 import argparse
 import os
 import urllib.parse
+import urllib.request
 import platform
+import ssl
 args = argparse.ArgumentParser()
 args.add_argument("-t", "--threads", type=int, default=5, help="Number of threads (parallel downloads/requests) to use when downloading (default 5)")
 args.add_argument("-s", "--chunk-size", type=int, default=10, help="Chunk size in MB (default 10)")
@@ -43,6 +45,15 @@ class HeaderProcessor:
             key, value = line.decode().split(':', 1)
             self.headers[key.lower()] = value.strip()
         self._line_count += 1
+def http_get_nobody(url, headers={}):
+    r = urllib.request.Request(url, headers=headers, method='GET')
+    r = urllib.request.urlopen(r, context=ssl.create_default_context())
+    d = {
+        'status': r.getcode(),
+        'headers': r.headers
+    }
+    r.close()
+    return d
 def find_ca_cert_bundle_linux():
     common_paths = [
         "/etc/ssl/certs/ca-certificates.crt",  # Debian/Ubuntu
@@ -56,10 +67,11 @@ def find_ca_cert_bundle_linux():
             return path
     
     raise Exception("Could not find a suitable CA bundle for your OS")
-def https_get(url, headers={}, nobody=False):
+def http_get(url, headers={}):
     c = pycurl.Curl()
     c.setopt(pycurl.URL, url)
     if platform.system() == 'Linux':
+        # a fix for pycurl on Linux
         c.setopt(pycurl.CAINFO, find_ca_cert_bundle_linux())
     c.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36')
     rheaders = HeaderProcessor()
@@ -67,10 +79,7 @@ def https_get(url, headers={}, nobody=False):
     for key, value in headers.items():
         c.setopt(pycurl.HTTPHEADER, [key + ': ' + value])
     d = io.BytesIO()
-    if nobody:
-        c.setopt(pycurl.NOBODY, 1)
-    else:
-        c.setopt(pycurl.WRITEDATA, d)
+    c.setopt(pycurl.WRITEDATA, d)
     c.perform()
     return {
         'status': c.getinfo(pycurl.HTTP_CODE),
@@ -81,7 +90,7 @@ def get_chunk(url, OUTPUT_FILE, offset, length, index):
     global write_index
     global running
     global total_received
-    r = https_get(url, headers={'Range': 'bytes=%d-%d' % (offset, offset + length - 1)})
+    r = http_get(url, headers={'Range': 'bytes=%d-%d' % (offset, offset + length - 1)})
     if r['status'] != 206:
         raise Exception('Unexpected status %d' % r['status'])
     while write_index != index:
@@ -90,7 +99,7 @@ def get_chunk(url, OUTPUT_FILE, offset, length, index):
     total_received += len(r['body'])
     write_index += 1
     running -= 1
-r = https_get(args.url, nobody=True)
+r = http_get_nobody(args.url)
 if r['status'] != 200:
     raise Exception('Unexpected status %d' % r['status'])
 if not 'accept-ranges' in r['headers'] or not 'bytes' in r['headers']['accept-ranges']:
@@ -100,7 +109,7 @@ START_TIME = time.time()
 TOTAL_SIZE = int(r['headers']['content-length'])
 OUTPUT_FILENAME = args.output
 if not OUTPUT_FILENAME:
-    OUTPUT_FILENAME = os.path.basename(urllib.parse.urlparse(args.url).path)
+    OUTPUT_FILENAME = urllib.parse.unquote(os.path.basename(urllib.parse.urlparse(args.url).path))
 OUTPUT_FILE = open(OUTPUT_FILENAME, 'wb')
 running = 0
 offset = 0
@@ -126,7 +135,7 @@ def status_update():
 threading.Thread(target=status_update, daemon=True).start()
 while offset <= TOTAL_SIZE:
     running += 1
-    threading.Thread(target=get_chunk, args=(args.url, OUTPUT_FILE, int(offset), CHUNK_SIZE, int(index)), daemon=True).start()
+    threading.Thread(target=get_chunk, args=(args.url, OUTPUT_FILE, int(offset), CHUNK_SIZE if offset + CHUNK_SIZE <= TOTAL_SIZE else TOTAL_SIZE - offset, int(index)), daemon=True).start()
     index += 1
     offset += CHUNK_SIZE
     while running >= args.threads:
